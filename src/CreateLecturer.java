9f42c042553e310dc42e43733c39252af9ad7443
ndaboom <ndabosam084@gmail.com>
2022-12-01 01:51:54 +0100

    Create Student & Lecturer & Main view added

---
+++ src/CreateLecturer.java
@@ -0,0 +1,111 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Font;
+import java.awt.HeadlessException;
+
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
+import java.awt.event.ActionEvent;
+
+@SuppressWarnings("serial")
+public class CreateLecturer extends JFrame{
+
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	Statement stmt;
+    Connexion maConnexion= new Connexion();
+	/**
+	 * Create the application.
+	 */
+	public CreateLecturer() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		this.setTitle("Lecturer registration");
+		this.setBounds(100, 100, 450, 357);
+		this.setLocationRelativeTo(null);
+		this.getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.WHITE);
+		panel.setBounds(0, 0, 434, 318);
+		this.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Lecturer registration");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblNewLabel.setBounds(154, 11, 124, 35);
+		panel.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(86, 83, 270, 29);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("Student name");
+		lblNewLabel_1.setBounds(84, 61, 84, 14);
+		panel.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Roll Number");
+		lblNewLabel_2.setBounds(88, 122, 78, 14);
+		panel.add(lblNewLabel_2);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(86, 143, 270, 29);
+		panel.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel_3 = new JLabel("Student Class");
+		lblNewLabel_3.setBounds(86, 183, 89, 14);
+		panel.add(lblNewLabel_3);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(86, 207, 270, 29);
+		panel.add(textField_2);
+		textField_2.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Submit");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				 String name_recup = textField.getText().trim();
+			     String rn_recup = textField_1.getText().trim();
+			     String student_class = textField_2.getText().trim();
+			     
+			     if(textField.getText().trim().length()>0 && textField_1.getText().toUpperCase().trim().length()>0 && textField_2.getText().toUpperCase().trim().length()>0) {
+			    	 try{
+				            String requete="INSERT INTO students_tb(roll_number, student_class, student_names)value ('"+rn_recup+"','"+name_recup+"','"+student_class+"')";
+				            stmt=maConnexion.ObtenirConnexion().createStatement();
+				            stmt.executeUpdate(requete);
+				            JOptionPane.showMessageDialog(null,"Student registered successfully!");
+				            textField.setText("");
+				            textField_1.setText("");
+				            textField_2.setText("");
+				            }catch(SQLException | HeadlessException e1) {
+				            	JOptionPane.showMessageDialog(null, "Something went wrong");
+				            	System.out.println(e1);
+				            }
+			     }else {
+			    	 JOptionPane.showMessageDialog(null, "Please fill in all required fields");
+			     }
+			        
+			}
+		});
+		btnNewButton.setBounds(154, 266, 124, 41);
+		panel.add(btnNewButton);
+	}
+}
---
+++ src/MainView.java
@@ -0,0 +1,294 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Font;
+import java.awt.HeadlessException;
+import java.util.Date;
+
+import javax.swing.JTextField;
+
+import com.toedter.calendar.JDateChooser;
+
+import net.proteanit.sql.DbUtils;
+
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.awt.event.ActionEvent;
+import javax.swing.JTable;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+
+public class MainView {
+
+	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
+	private com.toedter.calendar.JDateChooser product_expiry_date;
+	private JLabel lblNewLabel_3;
+	private JLabel lblNewLabel_4;
+	private JTextField textField_2;
+	private JLabel lblNewLabel_5;
+	private JTextField textField_3;
+	private JLabel lblNewLabel_6;
+	private JTextField textField_4;
+	private javax.swing.JScrollPane jScrollPane1;
+	
+	 private javax.swing.JMenu jMenu1;
+	    private javax.swing.JMenu jMenu2;
+	    private javax.swing.JMenu jMenu3;
+	    private javax.swing.JMenu jMenu4;
+	    private javax.swing.JMenu jMenu5;
+	    private javax.swing.JMenu jMenu6;
+	    private javax.swing.JMenuBar jMenuBar1;
+	    private javax.swing.JMenuItem jMenuItem1;
+	    private javax.swing.JMenuItem jMenuItem2;
+	    private javax.swing.JMenuItem jMenuItem3;
+	    private javax.swing.JMenuItem jMenuItem4;
+	    private javax.swing.JMenuItem jMenuItem5;
+	    private javax.swing.JMenuItem jMenuItem6;
+	    private javax.swing.JMenuItem jMenuItem7;
+	    private javax.swing.JMenuItem jMenuItem8;
+	    private javax.swing.JPanel jPAppli;
+	    private javax.swing.JPopupMenu.Separator jSeparator1;
+	    private javax.swing.JPopupMenu.Separator jSeparator2;
+	    private javax.swing.JPopupMenu.Separator jSeparator3;
+	    private javax.swing.JPopupMenu.Separator jSeparator4;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		// Look and feel 
+				try {
+		            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+		                if ("Nimbus".equals(info.getName())) {
+		                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+		                    break;
+		                }
+		            }
+		        } catch (ClassNotFoundException ex) {
+		            java.util.logging.Logger.getLogger(MainView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		        } catch (InstantiationException ex) {
+		            java.util.logging.Logger.getLogger(MainView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		        } catch (IllegalAccessException ex) {
+		            java.util.logging.Logger.getLogger(MainView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+		            java.util.logging.Logger.getLogger(MainView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+		        }
+				// Look and feel
+				
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainView window = new MainView();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MainView() {
+		initialize();
+		getData();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	Statement stmt;
+    Connexion maConnexion= new Connexion();
+    private JTable table;
+    
+    public void getData(){
+    	System.out.println("Fetching data...");
+        try{
+
+            java.sql.Statement stmt1= maConnexion.ObtenirConnexion().createStatement();
+            java.sql.ResultSet resultat= stmt1.executeQuery("SELECT * FROM evaluations_record");
+            table.setModel(DbUtils.resultSetToTableModel(resultat));
+	
+	
+       }catch(Exception e){
+	
+	
+       }
+    }
+	private void initialize() {
+		
+		
+		frame = new JFrame();
+		frame.setBounds(100, 100, 1000, 452);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, 984, 413);
+		panel.setBackground(Color.WHITE);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Lecturer's Evaluation");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel.setBounds(265, 0, 191, 29);
+		panel.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Lecturer's name");
+		lblNewLabel_1.setBounds(10, 47, 91, 14);
+		panel.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Course name");
+		lblNewLabel_2.setBounds(10, 81, 81, 14);
+		panel.add(lblNewLabel_2);
+		
+		textField = new JTextField();
+		textField.setBounds(139, 40, 213, 29);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(139, 74, 212, 29);
+		panel.add(textField_1);
+		textField_1.setColumns(10);
+		
+		product_expiry_date = new JDateChooser();
+		product_expiry_date.setDate(new Date());
+		product_expiry_date.setDateFormatString("yyyy-MM-dd");
+		product_expiry_date.setBounds(139, 114, 213, 29);
+		panel.add(product_expiry_date);
+		
+		lblNewLabel_3 = new JLabel("Date");
+		lblNewLabel_3.setBounds(10, 129, 46, 14);
+		panel.add(lblNewLabel_3);
+		
+		lblNewLabel_4 = new JLabel("Knowledge and Competence :");
+		lblNewLabel_4.setBounds(22, 168, 183, 14);
+		panel.add(lblNewLabel_4);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(215, 154, 137, 42);
+		panel.add(textField_2);
+		textField_2.setColumns(10);
+		
+		lblNewLabel_5 = new JLabel("Teaching skills/ Methodology :");
+		lblNewLabel_5.setBounds(22, 220, 173, 14);
+		panel.add(lblNewLabel_5);
+		
+		textField_3 = new JTextField();
+		textField_3.setBounds(215, 207, 137, 40);
+		panel.add(textField_3);
+		textField_3.setColumns(10);
+		
+		lblNewLabel_6 = new JLabel("Ethical Values :");
+		lblNewLabel_6.setBounds(22, 283, 146, 14);
+		panel.add(lblNewLabel_6);
+		
+		textField_4 = new JTextField();
+		textField_4.setBounds(215, 270, 137, 40);
+		panel.add(textField_4);
+		textField_4.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Save");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// Query
+				 SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+			        String evaluation_date_recup = sdf.format(product_expiry_date.getDate());
+			        String lecturer_name_recup = textField.getText().toUpperCase().trim();
+			        String course_name_recup = textField_1.getText().trim();
+			        String knowledge_points_recup = textField_2.getText().trim();
+			        String teaching_skills_recup = textField_3.getText().trim();
+			        String ethical_values_recup = textField_4.getText().trim();
+			        
+			        if(textField_1.getText().trim().length()>0 && textField.getText().toUpperCase().trim().length()>0 && textField_2.getText().toUpperCase().trim().length()>0
+			        		&& textField_3.getText().trim().length()>0 && textField_4.getText().trim().length()>0){
+			            
+			            try{
+			            String requete="INSERT INTO evaluations_record(lecturer_name, course_name, evaluation_date, knowledge_points, teaching_points, ethical_points)value ('"+lecturer_name_recup+"','"+course_name_recup+"','"+evaluation_date_recup+"','"+knowledge_points_recup+"','"+teaching_skills_recup+"','"+ethical_values_recup+"')";
+			            stmt=maConnexion.ObtenirConnexion().createStatement();
+			            stmt.executeUpdate(requete);
+			            getData();
+			            JOptionPane.showMessageDialog(null,"Record saved successfully!");
+			            textField.setText("");
+			            textField_1.setText("");
+			            textField_2.setText("");
+			            textField_3.setText("");
+			            textField_4.setText("");
+			        product_expiry_date.setDate(new Date());
+			            }catch(SQLException | HeadlessException e1) {
+			            	JOptionPane.showMessageDialog(null, "Something went wrong");
+			            	System.out.println(e1);
+			            }
+			        }else {
+			        	JOptionPane.showMessageDialog(null, "Please fill in all required fields");
+			        }
+				//Query
+			}
+		});
+		btnNewButton.setBounds(118, 347, 152, 40);
+		panel.add(btnNewButton);
+		
+		jScrollPane1 = new javax.swing.JScrollPane();
+		jScrollPane1.setSize(549, 347);
+		jScrollPane1.setLocation(414, 40);
+		
+		table = new JTable();
+		table.setBounds(370, 384, 489, -343);
+		table.setModel(new javax.swing.table.DefaultTableModel(
+	            new Object [][] {
+	                {null, null, null, null, null, null, null, null, null},
+	                {null, null, null, null, null, null, null, null, null},
+	                {null, null, null, null, null, null, null, null, null},
+	                {null, null, null, null, null, null, null, null, null}
+	            },
+	            new String [] {
+	                "Lecturer name", "Course name", "Knowledge & Competence", "Teaching skills", "Ethical values"
+	            }
+	        ));
+		table.addMouseListener(new java.awt.event.MouseAdapter() {
+	            public void mouseClicked(java.awt.event.MouseEvent evt) {
+	                //jTable1MouseClicked(evt);
+	            }
+	        });
+	    jScrollPane1.setViewportView(table);
+		panel.add(jScrollPane1);
+		
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBounds(0, 0, 101, 22);
+		panel.add(menuBar);
+		
+		JMenu mnNewMenu = new JMenu("File");
+		menuBar.add(mnNewMenu);
+		
+		JMenuItem mntmNewMenuItem = new JMenuItem("Add Student");
+		mntmNewMenuItem.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) { 
+            System.out.println("Clicked on");
+            CreateStudent ct = new CreateStudent();
+            ct.setVisible(true);
+            }
+		});
+		mnNewMenu.add(mntmNewMenuItem);
+		
+		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Add Lecturer");
+		mnNewMenu.add(mntmNewMenuItem_1);
+		
+		
+	
+	}
+}
